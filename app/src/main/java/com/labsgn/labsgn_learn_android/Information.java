aaa84f9d54825133338e063beac265d641399871
rhonabdullah <rhonabdullah95@gmail.com>
2016-02-24 17:59:56 +0700

    Recycler View - PART I (Setup)

---
+++ app/src/main/java/com/labsgn/labsgn_learn_android/Information.java
@@ -0,0 +1,10 @@
+package com.labsgn.labsgn_learn_android;
+
+/**
+ * Created by rhony on 24/02/16.
+ * Todo: 6. Membuat class baru untuk sementara menyimpan informasi recyclerView
+ */
+public class Information {
+    public  int iconId;
+    public  String title;
+}
--- app/src/main/java/layout/MainFragment.java
+++ app/src/main/java/layout/MainFragment.java
@@ -5,38 +5,72 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.labsgn.labsgn_learn_android.Adapter;
+import com.labsgn.labsgn_learn_android.Information;
 import com.labsgn.labsgn_learn_android.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
- *
  */
 public class MainFragment extends Fragment {
 
+    //Todo 4. Menambahkan instance RecyclerView milik android.v7
+    private RecyclerView recyclerView;
     private ActionBarDrawerToggle mDrawerToggle;
     private DrawerLayout mDrawerLayout;
 
+    //Todo 20. Membuat Instance variable adapter baru
+    private Adapter myAdapter;
 
     public MainFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.main_fragment, container, false);
+
+        // Todo 5. Menghubungkan instance recyclerView dengan recyclerView yang ada didalam main_fragment
+        View layout = inflater.inflate(R.layout.main_fragment, container, false);
+        recyclerView = (RecyclerView) layout.findViewById(R.id.drawerList);
+
+        //Todo 24. Simpan instance myAdapter kemudian set adapter ke recyclerView
+        myAdapter = new Adapter(getActivity(), getData());
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return layout;
+    }
+
+    //Todo 21. Membuat method untuk mengambil data
+    public static List<Information> getData() {
+        List<Information> data = new ArrayList<>();
+        int icons[] = {R.drawable.ic_looks_one_black_48dp, R.drawable.ic_looks_two_black_48dp, R.drawable.ic_looks_3_black_48dp};
+        String titles[] ={"Dji","Sam","Soe"};
+
+        //Todo 22. Melakukan perulangan untuk menyimpan data ke dalam Information
+        for (int i = 0; i<titles.length && i<icons.length; i++){
+            Information current = new Information();
+            current.iconId = icons[i];
+            current.title = titles[i];
+            data.add(current);
+        }
+        return data;
     }
 
     public void setUp(DrawerLayout drawerLayout, final Toolbar toolbar) {
         mDrawerLayout = drawerLayout;
-        mDrawerToggle = new ActionBarDrawerToggle(getActivity(),mDrawerLayout,toolbar, R.string.drawer_open, R.string.drawer_close){
+        mDrawerToggle = new ActionBarDrawerToggle(getActivity(), mDrawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close) {
 
             @Override
             public void onDrawerOpened(View drawerView) {
@@ -55,7 +89,7 @@ public class MainFragment extends Fragment {
             @Override
             public void onDrawerSlide(View drawerView, float slideOffset) {
                 super.onDrawerSlide(drawerView, slideOffset);
-                if (slideOffset < 0.6 ) {
+                if (slideOffset < 0.6) {
                     toolbar.setAlpha(1 - slideOffset);
                 }
             }
